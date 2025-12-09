d80c497808e84976d002295d6eeca8e549751f73
NGxDTV <info@ngxd-tv.de>
2025-12-09 21:40:06 +0100

    Add Search module to NGTube package
    Updated the package docstring to include searches and imported the Search module in the __init__.py file to expose search functionality.

--- NGTube/__init__.py
+++ NGTube/__init__.py
@@ -1,13 +1,14 @@
 """
 NGTube - A Python library for YouTube data extraction
 
-This package provides modules for extracting data from YouTube videos and channels.
+This package provides modules for extracting data from YouTube videos, channels, comments, and searches.
 """
 
 from .core import YouTubeCore
 from .video.video import Video
 from .comments.comments import Comments
 from .channel.channel import Channel
+from .search.search import Search
 
 __version__ = "1.0.0"
 __author__ = "NGTube Team"
